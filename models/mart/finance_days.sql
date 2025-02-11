6992d56cbeb40ff255a99aedd30c819606f6f91e
giliberti.matthieu@gmail.com <giliberti.matthieu@gmail.com>
2025-02-11 16:12:46 +0000

    add

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,11 +1,12 @@
 select 
 date_date,
-
+sum(nb_trans) as nb_transactions,
 sum(revenue) as total_revenue,
-sum(revenue)/sum(qty) as avg_basket,
+sum(revenue)/sum(nb_trans) as avg_basket,
 sum(margin_op) as margin_op,
 sum(purchase_cost) as total_purchase_cost,
-sum(margin),
+sum(shipping_fee) as total_shipping_fee,
+sum(logcost) as total_logcost,
 sum(qty) as total_quantity_sold
 from {{ref("int_sales_margin")}}
 group by date_date
