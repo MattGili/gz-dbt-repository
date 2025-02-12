de1b716a83982eb784a63ab75d9084c50a7a08f8
giliberti.matthieu@gmail.com <giliberti.matthieu@gmail.com>
2025-02-12 14:20:23 +0000

    add

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,16 @@
+SELECT 
+finance.date_date,
+round(finance.margin_op-campaign.ads_cost,0) as ads_margin,
+round(finance.avg_basket,1) as avg_basket,
+round(finance.margin_op,0) as margin_op,
+campaign.ads_cost,
+campaign.ads_impression,
+campaign.ads_click,
+finance.total_quantity_sold,
+round(finance.total_revenue,1) as total_revenue,
+round(finance.total_purchase_cost,1) as total_purchase_cost,
+round(finance.total_shipping_fee,1) as total_shipping_fee,
+round(finance.total_logcost,1) as total_logcost
+FROM {{ref("finance_days")}} as finance
+LEFT JOIN {{ref("int_campaigns_day")}} as campaign
+ON campaign.date_date = finance.date_date
